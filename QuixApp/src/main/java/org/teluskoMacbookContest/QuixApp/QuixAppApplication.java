3b84b98ad1c5b1ad8a30fa63d4b97cb414d15d4b
ManikantaMandala <mandala.manikanta.20cse@bmu.edu.in>
2023-05-30 00:31:26 +0530

    Downloaded Spring mvc folder

---
+++ QuixApp/src/main/java/org/teluskoMacbookContest/QuixApp/QuixAppApplication.java
@@ -0,0 +1,13 @@
+package org.teluskoMacbookContest.QuixApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuixAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuixAppApplication.class, args);
+	}
+
+}
---
+++ QuixApp/src/test/java/org/teluskoMacbookContest/QuixApp/QuixAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teluskoMacbookContest.QuixApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuixAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
